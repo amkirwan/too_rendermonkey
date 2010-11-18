3ecffd91f22381ab0f4b3e5b2c8e319677683653
Anthony Kirwan <amkirwan@gmail.com>
2010-11-18 16:56:21 -0500

    moved init.rb to rails/init.rb

---
+++ rails/init.rb
@@ -0,0 +1,12 @@
+# Include hook code here   
+
+  puts "&&&&&&&&&&&&&&&&&&&&&&&&&&"
+
+unless ActionController::Base.instance_methods.include? "render_with_rendermonkey_too"
+  require 'too_rendermonkey'
+  require 'too_rendermonkey_css'
+  ActionController::Base.send :include, TooRendermonkey
+  ActionView::Base.send :include, TooRendermonkeyCss
+end   
+
+Mime::Type.register 'application/pdf', :pdf
